83aa22ebbd3865c3387f27aeeccd3640c9a56e90
Alactraz <samyn.frederic@gmail.com>
2024-10-28 11:38:53 +0100

    1.2 création de l'API RESTFULL à partir du cour

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,24 +1,47 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+const Thing = require('./models/thing.js');  // Importation du modèle
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
+app.use(express.json());  // Middleware pour gérer le JSON
+
+// Connexion mongodb 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://alcatraz0941:Fr3d3r1c@cluster0.ey4lw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Route POST pour /api/stuff
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+    .catch(error => res.status(400).json({ error }));
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+// Route GET ppur /api/stuff 
+app.use('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
